1c58a5d2a723a10ba3713a74a46518f0a3ced1d8
ericmuthoka <muthokaerick@gmail.com>
2023-06-05 04:21:54 -0700

    Python world

--- 0x00-python-hello_world/lists.h
+++ 0x00-python-hello_world/lists.h
@@ -1,10 +1,6 @@
 #ifndef LISTS_H
 #define LISTS_H
 
-typedef struct listint_s {
-    int data;
-    struct listint_s *next;
-} listint_t;
 
 int check_cycle(listint_t *list);
 int check_cycle(listint_t *list);
