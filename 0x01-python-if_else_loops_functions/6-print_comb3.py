2e6670c748dd0989b646c1549036ee6f711d51f1
ericmuthoka <muthokaerick@gmail.com>
2023-06-06 14:42:44 -0700

    python if else loops

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-for x in range(0, 10):
-    for y in range(x + 1, 10):
-        if x == 8 and y == 9:
-            print('89')
-        else:
-            print('{}{}, '.format(x, y), end='')
+for i in range(0, 10):
+    for j in range(i + 1, 10):
+        if i != 8:
+            print("{}{}, ".format(i, j), end="")
+print("89")
