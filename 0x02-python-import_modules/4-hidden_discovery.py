90cc77ee3e45e133fe04d2b712ca0386afbaf0aa
ericmuthoka <muthokaerick@gmail.com>
2023-06-08 09:01:41 -0700

    python modules

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+
 if __name__ == "__main__":
     """Print all names defined by hidden_4 module."""
     import hidden_4
@@ -7,3 +8,4 @@ if __name__ == "__main__":
     for name in names:
         if name[:2] != "__":
             print(name)
+
