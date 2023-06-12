6e98895bed514ad05fed6f453d65c2e353783f6f
ericmuthoka <muthokaerick@gmail.com>
2023-06-12 12:35:12 -0700

    python data structures

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -5,22 +5,23 @@
  * @head: pointer to head
  * Return: 0 if it is not a palindrome, 1 if it is a palindrome
  */
-int is_palindrome(listint_t** head)
+int is_palindrome(listint_t **head)
 {
 	if (*head == NULL || (*head)->next == NULL)
 	{
 		return (1);
 	}
 
-	listint_t* slow = *head;
-	listint_t* fast = *head;
-	listint_t* prev = NULL;
+	listint_t *slow = *head;
+	listint_t *fast = *head;
+	listint_t *prev = NULL;
 
 	while (fast != NULL && fast->next != NULL)
 	{
 		fast = fast->next->next;
 
-		listint_t* next = slow->next;
+		listint_t *next = slow->next;
+
 		slow->next = prev;
 		prev = slow;
 		slow = next;
