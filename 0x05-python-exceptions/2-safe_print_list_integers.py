f4568bba75222e9796d2b05f788caadfcaef5f87
ericmuthoka <muthokaerick@gmail.com>
2023-06-26 14:09:42 -0700

    stacks and queues

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,17 +1,11 @@
 #!/usr/bin/python3
 
 def safe_print_list_integers(my_list=[], x=0):
-
-    count = 0  # Counter for the number of integers printed
-    try:
-        for item in my_list[:x]:
-            try:
-                print("{:d}".format(item), end=' ')
-                count += 1
-            except ValueError:
-                pass  # Skip non-integer values silently
-    except TypeError:
-        raise Exception("x must be an integer")
-
-    print()  # Print a new line
+    count = 0
+    for i in range(x):
+        try:
+            print("{:d}".format(my_list[i]))
+            count += 1
+        except (ValueError, TypeError):
+            pass
     return count
