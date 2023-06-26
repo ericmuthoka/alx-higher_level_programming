6f6bdbe3db68c5094ffd59bb42434099c18fbb6a
ericmuthoka <muthokaerick@gmail.com>
2023-06-26 14:26:14 -0700

    stacks and queues

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,18 +1,19 @@
 #!/usr/bin/python3
 
-for i in range(list_length):
-    try:
-        result = 0
-        if i < len(my_list_1) and i < len(my_list_2):
-            if my_list_2[i] != 0:
-                result = my_list_1[i] / my_list_2[i]
-            else:
-                print("division by 0")
-        else:
+def list_division(my_list_1, my_list_2, list_length):
+    new_list = []
+    for i in range(0, list_length):
+        try:
+            result = my_list_1[i] / my_list_2[i]
+        except TypeError:
+            print("wrong type")
+            result = 0
+        except ZeroDivisionError:
+            print("division by 0")
+            result = 0
+        except IndexError:
             print("out of range")
-    except TypeError:
-        print("wrong type")
-    finally:
-        new_list.append(result)
-
-return new_list
+            result = 0
+        finally:
+            new_list.append(result)
+    return new_list
