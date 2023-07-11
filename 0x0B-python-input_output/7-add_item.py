4de101daed6e2e63a622b1deaaeac750f1cb64cf
ericmuthoka <muthokaerick@gmail.com>
2023-07-11 10:33:14 -0700

    0x0B-python-input_output

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,17 +1,29 @@
 #!/usr/bin/python3
-"""Script that adds command-line args and saves them to a file."""
+
+from sys import argv
+from os.path import exists
+from json import load, dump
 
 
-import sys
+def save_to_json_file(my_obj, filename):
+    """Save an object to a JSON file."""
+    with open(filename, "w") as f:
+        dump(my_obj, f)
 
-if __name__ == "__main__":
-    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-    load_from_json_file = \
-        __import__('6-load_from_json_file').load_from_json_file
 
-    try:
-        items = load_from_json_file("add_item.json")
-    except FileNotFoundError:
-        items = []
-    items.extend(sys.argv[1:])
-    save_to_json_file(items, "add_item.json")
+def load_from_json_file(filename):
+    """Load an object from a JSON file."""
+    with open(filename) as f:
+        return load(f)
+
+
+filename = "add_item.json"
+
+if exists(filename):
+    items = load_from_json_file(filename)
+else:
+    items = []
+
+items.extend(argv[1:])
+
+save_to_json_file(items, filename)
