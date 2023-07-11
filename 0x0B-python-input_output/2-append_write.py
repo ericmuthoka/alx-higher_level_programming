de9b97b25bc274fbe15e5542b98a83d611ec84d7
ericmuthoka <muthokaerick@gmail.com>
2023-07-11 10:20:34 -0700

    python

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,7 +1,16 @@
 #!/usr/bin/python3
+"""Defines a function for appending a string to a UTF8 text file."""
+
 
 def append_write(filename="", text=""):
-    """Append a string to a UTF-8 text file and return the character count."""
-    with open(filename, mode="a", encoding="utf-8") as f:
-        f.write(text)
-        return len(text)
+    """Append a string to the end of a UTF8 text file.
+
+    Args:
+        filename (str): The name of the file to append.
+        text (str): The string to append to the file.
+
+    Returns:
+        int: The number of characters appended.
+    """
+    with open(filename, "a", encoding="utf-8") as f:
+        return f.write(text)
