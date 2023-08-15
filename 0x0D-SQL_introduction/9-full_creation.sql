b390249f9606472d14369982380be99dc13deee2
ericmuthoka <muthokaerick@gmail.com>
2023-08-15 04:10:43 -0700

    0x0D-SQL_introduction

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,17 +1,6 @@
--- Create a table named second_table with columns id, name, and score in the MySQL server.
-CREATE TABLE IF NOT EXISTS `second_table` (
-    `id` INT,
-    `name` VARCHAR(256),
-    `score` INT
-);
-
--- Insert multiple rows into the second_table.
-INSERT INTO `second_table` (`id`, `name`, `score`) VALUES
-    (1, "John", 10),
-    (2, "Alex", 3),
-    (3, "Bob", 14),
-    (4, "George", 8);
-
--- Retrieve and display the data from the second_table.
-SELECT * FROM `second_table`;
-
+-- Creates and fills a table second_table with attributes id, name and score in my MySQL server adding many rows
+CREATE TABLE IF NOT EXISTS `second_table` (`id` INT, `name` VARCHAR(256), `score` INT);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (1, "John", 10);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (2, "Alex", 3);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (3, "Bob", 14);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (4, "George", 8);
