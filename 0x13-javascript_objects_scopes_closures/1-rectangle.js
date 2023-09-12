91428a56774a050205fca6bba6b12a5f7259ec1c
ericmuthoka <muthokaerick@gmail.com>
2023-09-12 15:24:52 -0700

    0x13-javascript_objects_scopes_closures

--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -1,14 +1,8 @@
 #!/usr/bin/node
-/**
- * Represents a rectangle with width and height attributes.
- * @class
- */
 class Rectangle {
-  constructor(w, h) {
+  constructor (w, h) {
     this.width = w;
     this.height = h;
   }
 }
-
 module.exports = Rectangle;
-
