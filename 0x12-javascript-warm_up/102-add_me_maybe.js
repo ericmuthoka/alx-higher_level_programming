ad743ccdb9d56c31d4670b9d9ff4991a010738fe
ericmuthoka <muthokaerick@gmail.com>
2023-09-11 17:41:08 -0700

    0x12-javascript-warm_up

--- 0x12-javascript-warm_up/102-add_me_maybe.js
+++ 0x12-javascript-warm_up/102-add_me_maybe.js
@@ -6,9 +6,3 @@ function addMeMaybe(number, theFunction) {
 }
 
 module.exports.addMeMaybe = addMeMaybe; // Export the function
-
-// Example usage:
-// const addMeMaybe = require('./102-add_me_maybe').addMeMaybe;
-// addMeMaybe(4, function (nb) {
-//   console.log('New value: ' + nb);
-// });
