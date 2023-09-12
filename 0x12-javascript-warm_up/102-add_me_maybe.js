ed8a32169739fbda7e421fe453209021f943a09f
ericmuthoka <muthokaerick@gmail.com>
2023-09-11 17:44:03 -0700

    0x12-javascript-warm_up

--- 0x12-javascript-warm_up/102-add_me_maybe.js
+++ 0x12-javascript-warm_up/102-add_me_maybe.js
@@ -1,9 +1,14 @@
-function incrementAndCall(number, theFunction) {
-  number += 1;
+#!/usr/bin/node
+
+function addMeMaybe(number, theFunction) {
+  number++;
   theFunction(number);
 }
 
+module.exports.addMeMaybe = addMeMaybe; // Export the function
+
 // Example usage:
-incrementAndCall(4, function (nb) {
-  console.log('New value: ' + nb);
-});
+// const addMeMaybe = require('./102-add_me_maybe').addMeMaybe;
+// addMeMaybe(4, function (nb) {
+//   console.log('New value: ' + nb);
+// });
