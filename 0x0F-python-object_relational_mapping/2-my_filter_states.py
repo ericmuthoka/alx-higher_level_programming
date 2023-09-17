beda536c28f772f9623a52786736d09a08de8b11
ericmuthoka <muthokaerick@gmail.com>
2023-09-17 02:25:13 -0700

    0x0F-python-object_relational_mapping

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,30 +1,38 @@
 #!/usr/bin/python3
 """
-This script takes a state name as an argument and displays all values in the
-'states' table where the 'name' matches the provided state name from the
-'hbtn_0e_0_usa' database.
+This script connects to a MySQL database and displays states from the 'states'
+table that match the provided state name.
 """
 
-import MySQLdb as db
-from sys import argv
+import sys
+import MySQLdb
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     """
-    Connect to the MySQL database and retrieve the states
-    where the 'name' matches the provided state name.
+    Connect to the MySQL database and retrieve states that match the provided
+    state name.
     """
-    db_connection = db.connect(host="localhost", port=3306,
-            user=argv[1], passwd=argv[2], db=argv[3])
-    db_cursor = db_connection.cursor()
+    try:
+        # Connect to the MySQL server
+        db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                             passwd=sys.argv[2], db=sys.argv[3])
+        cursor = db.cursor()
 
-    query = (
-            "SELECT * FROM states WHERE name LIKE BINARY '{}' "
-            "ORDER BY states.id ASC"
-            .format(argv[4])
-            )
-    db_cursor.execute(query)
+        # Create the SQL query using the provided state name
+        query = "SELECT * FROM states WHERE name LIKE BINARY '{}' ORDER BY id"\
+                .format(sys.argv[4])
 
-    selected_rows = db_cursor.fetchall()
+        # Execute the query
+        cursor.execute(query)
 
-    for row in selected_rows:
-        print(row)
+        # Fetch all the results and display them
+        results = cursor.fetchall()
+        for row in results:
+            print(row)
+
+        # Close the cursor and the database connection
+        cursor.close()
+        db.close()
+    except MySQLdb.Error as e:
+        print("Error: Unable to connect to the MySQL server:", str(e))
