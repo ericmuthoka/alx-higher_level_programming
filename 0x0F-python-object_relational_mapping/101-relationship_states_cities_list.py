c499d6e3b610a8475425084a77563e158c80d888
ericmuthoka <muthokaerick@gmail.com>
2023-09-17 11:40:11 -0700

    0x0F-python-object_relational_mapping

---
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""
+This script lists all State objects and their corresponding City objects
+from the database hbtn_0e_101_usa.
+"""
+
+import sys
+from model_state import Base, State
+from model_city import City
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        print("Usage: {} <username> <password> <database>".format(sys.argv[0]))
+        sys.exit(1)
+
+    # Extract MySQL credentials and database name
+    username, password, database = sys.argv[1], sys.argv[2], sys.argv[3]
+
+    # Create the connection string
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'.format(username, password, database),
+        pool_pre_ping=True
+    )
+
+    # Create a configured "Session" class
+    Session = sessionmaker(bind=engine)
+
+    # Create a Session instance
+    session = Session()
+
+    # Query to retrieve State and City objects
+    states = session.query(State).order_by(State.id)
+    for state in states:
+        print("{}: {}".format(state.id, state.name))
+        for city in state.cities:
+            print("\t{}: {}".format(city.id, city.name))
+
+    # Close the session
+    session.close()
