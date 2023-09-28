593039b32257da23304ed9c961601bb744e580f2
ericmuthoka <muthokaerick@gmail.com>
2023-09-28 11:16:56 -0700

    0x10-python-network_0

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,12 +1,3 @@
 #!/bin/bash
 # This script retrieves the size of the response body in bytes for a given URL using curl.
-
-# Check if the user provided a URL
-if [ -z "$1" ]; then
-  echo "Usage: $0 <URL>"
-  exit 1
-fi
-
-# Send a GET request to the specified URL and display the size of the response body
-response_size=$(curl -sI "$1" | awk '/Content-Length/{print $2}')
-echo "$response_size"
+curl -s "$1" | wc -c
