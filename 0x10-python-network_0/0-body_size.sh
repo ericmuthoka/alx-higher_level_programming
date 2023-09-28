6158b94e93249746055b3f0fe62a893c8921210c
ericmuthoka <muthokaerick@gmail.com>
2023-09-28 10:31:07 -0700

    0x10-python-network_0

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,18 +1,12 @@
 #!/bin/bash
-# This script retrieves the Content-Length of a given IP address using curl and awk.
+# This script retrieves the size of the response body in bytes for a given URL using curl.
 
-# Check if the user provided an IP address
+# Check if the user provided a URL
 if [ -z "$1" ]; then
-  echo "Usage: $0 <IP_address>"
+  echo "Usage: $0 <URL>"
   exit 1
 fi
 
-# Send a HEAD request to the specified IP address and retrieve the Content-Length
-content_length=$(curl -sI "$1" | awk '/Content-Length/{print $2}')
-
-# Check if Content-Length was found
-if [ -z "$content_length" ]; then
-  echo "Content-Length not found for the given IP address."
-else
-  echo "Content-Length of the response: $content_length bytes"
-fi
+# Send a GET request to the specified URL and display the size of the response body
+response_size=$(curl -sI "$1" | awk '/Content-Length/{print $2}')
+echo "$response_size"
