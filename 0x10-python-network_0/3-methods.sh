540f0c8db143b9c007d8176bc934ebd952a16ff9
ericmuthoka <muthokaerick@gmail.com>
2023-09-28 11:00:37 -0700

    0x10-python-network_0

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,16 +1,3 @@
 #!/bin/bash
 # This script displays the HTTP methods accepted by a server for a given URL.
-
-# Check if the user provided a URL
-if [ -z "$1" ]; then
-  echo "Usage: $0 <URL>"
-  exit 1
-fi
-
-# Send an OPTIONS request to the specified URL and display the allowed methods
-allowed_methods=$(curl -sI -X OPTIONS "$1" | awk -F ": " '/Allow/ {print $2}')
-
-# Display the allowed HTTP methods
-echo "HTTP Methods allowed for $1:"
-echo "$allowed_methods"
-
+curl -sI "$1" | grep "Allow" | awk '{print $2, $3, $4}'
