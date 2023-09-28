c268b32cc79cd2a546f41dffc59f4b8ee573ce45
ericmuthoka <muthokaerick@gmail.com>
2023-09-28 11:09:05 -0700

    0x10-python-network_0

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
 # This script fetches the allowed HTTP methods for a given URL.
-# Usage: ./3-methods.sh <URL>
-
-# Retrieve the URL from the user.
-URL=$1
-
-# Send an HTTP HEAD request to the specified server and extract the allowed methods.
 curl -sI "$URL" | grep "Allow" | awk '{print $2, $3, $4}'
 
