f5784dac4edb68f2a44ca20ab7ad5760cc46d49e
ericmuthoka <muthokaerick@gmail.com>
2023-09-29 12:40:55 -0700

    0x11-python-network_1

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,17 +1,10 @@
 #!/usr/bin/python3
-"""
-Fetches the body response from https://alx-intranet.hbtn.io/status
-"""
+"""Fetches the body response from https://alx-intranet.hbtn.io/status."""
 
 import requests
 
 if __name__ == "__main__":
-    url = "https://alx-intranet.hbtn.io/status"
-
-    response = requests.get(url)
-    content_type = type(response.text).__name__
-    content = response.text
-
+    response = requests.get("https://alx-intranet.hbtn.io/status")
     print("Body response:")
-    print(f"    - type: {content_type}")
-    print(f"    - content: {content}")
+    print(f"\t- type: {type(response.text).__name__}")
+    print(f"\t- content: {response.text}")
