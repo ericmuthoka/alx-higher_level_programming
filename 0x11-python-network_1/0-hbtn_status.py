0748968cca76c5ef6387495a3da487da3fc6fc5b
ericmuthoka <muthokaerick@gmail.com>
2023-09-29 12:16:04 -0700

    0x11-python-network_1

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import urllib.request
+
+if __name__ == "__main__":
+    url = "https://alx-intranet.hbtn.io/status"
+
+    with urllib.request.urlopen(url) as response:
+        body = response.read()
+        print("Body response:")
+        print(f"    - type: {type(body)}")
+        print(f"    - content: {body}")
+        print(f"    - utf8 content: {body.decode('utf-8')}")
