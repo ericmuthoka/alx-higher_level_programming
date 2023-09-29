1484722b12b9a29498fbee7d0e56d395f1b19192
ericmuthoka <muthokaerick@gmail.com>
2023-09-29 12:24:47 -0700

    0x11-python-network_1

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,14 +1,12 @@
 #!/usr/bin/python3
-"""Fetches the body response from a URL using urllib."""
-
+"""Fetches the body response from https://alx-intranet.hbtn.io/status."""
 import urllib.request
 
 if __name__ == "__main__":
-    url = "https://alx-intranet.hbtn.io/status"
-
-    with urllib.request.urlopen(url) as response:
+    request = urllib.request.Request("https://alx-intranet.hbtn.io/status")
+    with urllib.request.urlopen(request) as response:
         body = response.read()
         print("Body response:")
-        print(f"    - type: {type(body)}")
-        print(f"    - content: {body}")
-        print(f"    - utf8 content: {body.decode('utf-8')}")
+        print("\t- Type: {}".format(type(body)))
+        print("\t- Content: {}".format(body))
+        print("\t- UTF-8 Content: {}".format(body.decode("utf-8")))
