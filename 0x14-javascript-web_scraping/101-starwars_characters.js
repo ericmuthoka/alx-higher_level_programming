878acd92b02e632ec4b130ab8a2c735e98330372
ericmuthoka <muthokaerick@gmail.com>
2023-10-24 12:11:11 -0700

    0x14-javascript-web_scraping

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -11,30 +11,6 @@ if (!movieId) {
 
 const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
 
-// Define the correct order of characters in "Return of the Jedi"
-const characterOrder = [
-  'Luke Skywalker',
-  'C-3PO',
-  'R2-D2',
-  'Darth Vader',
-  'Leia Organa',
-  'Obi-Wan Kenobi',
-  'Chewbacca',
-  'Han Solo',
-  'Jabba Desilijic Tiure',
-  'Wedge Antilles',
-  'Yoda',
-  'Palpatine',
-  'Boba Fett',
-  'Lando Calrissian',
-  'Ackbar',
-  'Mon Mothma',
-  'Arvel Crynyd',
-  'Wicket Systri Warrick',
-  'Nien Nunb',
-  'Bib Fortuna',
-];
-
 // Make a GET request to the Star Wars API to retrieve the movie details
 request.get(apiUrl, (error, response, body) => {
   if (error) {
@@ -42,26 +18,16 @@ request.get(apiUrl, (error, response, body) => {
   } else {
     if (response.statusCode === 200) {
       const movie = JSON.parse(body);
-      const characters = movie.characters;
+      const characterUrls = movie.characters;
 
-      // Create a map of character names to URLs
-      const characterUrlMap = {};
-      characters.forEach((characterUrl) => {
-        const characterId = characterUrl.split('/').slice(-2)[0];
-        characterUrlMap[characterId] = characterUrl;
-      });
-
-      // Print characters in the correct order
-      characterOrder.forEach((characterName) => {
-        const characterUrl = characterUrlMap[characterName];
-        if (characterUrl) {
-          request.get(characterUrl, (charError, charResponse, charBody) => {
-            if (!charError && charResponse.statusCode === 200) {
-              const character = JSON.parse(charBody);
-              console.log(character.name);
-            }
-          });
-        }
+      // Fetch and print the names of characters in order
+      characterUrls.forEach((characterUrl) => {
+        request.get(characterUrl, (charError, charResponse, charBody) => {
+          if (!charError && charResponse.statusCode === 200) {
+            const character = JSON.parse(charBody);
+            console.log(character.name);
+          }
+        });
       });
     } else {
       console.error(`Request failed with status code: ${response.statusCode}`);
